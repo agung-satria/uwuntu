45165f4b6853595f7ffb1de97a5f68eafef4bba6
Agung Satria <agungsatria890@gmail.com>
2022-06-29 15:47:57 +0700

    change slstatus color

--- suckless/slstatus/config.h
+++ suckless/slstatus/config.h
@@ -11,9 +11,9 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	{ run_command,	"^c#e79b6a^ %2s ", "sb-mpdsong" },
-	{ run_command,	"^c#ee8080^ %2s ", "sb-nettraf" },
+	{ run_command,	"^c#ebaf80^ %2s ", "sb-nettraf" },
 	{ run_command,	"^c#85bf69^ %2s ", "sb-battery" },
 	{ run_command,	"^c#89b8c2^ %2s ", "sb-volubuntu" },
-	{ run_command,	"^c#c094d2^ %2s ", "sb-diskusage" },
+	{ run_command,	"^c#e5d8ea^ %2s ", "sb-diskusage" },
 	{ run_command,	"^c#d5c07f^ %2s ", "sb-clock" },
 };
